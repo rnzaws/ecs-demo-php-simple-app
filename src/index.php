b20954095f4fc3ef7c46bde6914ab3061e3950ea
Ryan <ryannitz@amazon.com>
2017-07-18 00:37:30 -0400

    Simplify

--- src/index.php
+++ src/index.php
@@ -2,26 +2,18 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Simple PHP App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>body {margin-top: 40px; background-color: #333;}</style>
     </head>
 
     <body>
-        <div class="container">
-            <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
-                        echo fread($myfile,filesize("/var/www/my-vol/date"));
-                        fclose($myfile);
-                ?>
-
-            </div>
-        </div>
+        <h1>Simple PHP App</h1>
+        <h2>Congratulations</h2>
+        <p>Your PHP application is now running on a container in Amazon ECS.</p>
+        <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+        <?php
+          $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+          echo fread($myfile,filesize("/var/www/my-vol/date"));
+          fclose($myfile);
+        ?>
     </body>
 </html>
